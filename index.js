bf4c2448d7db2bcb1681d923b682be0c913c719c
ayerebeca95 <ayerebeca95@gmail.com>
2023-06-21 21:26:56 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+/* FECHA Y HORA
+const hoy = new Date();
+console.log(hoy);
+console.log(new Date());*/
+
+console.log("Hola, este es mi primer ejercicio con Node en el mejor Bootcamp de programación del mundo".);
+
