2fd74838a2914ef3c6ff378797bacda80c223e61
ayerebeca95 <ayerebeca95@gmail.com>
2023-06-22 20:20:49 -0400

    desafio 2

---
+++ desafio2.js
@@ -0,0 +1,14 @@
+
+const misDatos = {
+    nombre: "Rebeca",
+    edad:28,
+    desarrolladora: true,
+    nacimiento: new Date(10), 
+    libros: {
+        nombre:"El principito",
+        autor: "Gonzalo Paz", 
+        fecha: new Date(10)
+    }
+}
+
+console.log(misDatos);
--- index.js
+++ index.js
@@ -5,3 +5,20 @@ console.log(new Date());*/
 
 console.log("Hola, este es mi primer ejercicio con Node en el mejor Bootcamp de programación del mundo".);
 
+//string 
+"Hola mundo" // para escribir con letras de texto
+
+//number
+4;
+
+//booleanos
+true;
+false;
+
+//null, undefined, false, 0
+if (null) {
+    console.log("cumple")
+}else {
+    console.log("no cumple");
+}
+
